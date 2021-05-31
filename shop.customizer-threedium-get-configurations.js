fd6c572db4b9df31c1ee3cecbfc8d8e9a34f3fea
Joelthorner <joeltorner@gmail.com>
2021-05-31 12:58:33 +0200

    Refactoring and fix init model with diferent sole-vira 270|360 values

---
+++ shop.customizer-threedium-get-configurations.js
@@ -0,0 +1,257 @@
+/**
+ * @file Contains all the configuration logic of the 3D model before initializing it
+ * @author joelthorner
+ */
+
+var module = {
+  /**
+   * Create an object with the initial 3D model settings. 
+   * According to the options that are selected.
+   * Nomes es mostraran les parts que jo digui la resta s'ocultaran gracies al <override>.
+   * @example
+   * parts: {
+   *   show: ['PartName'],
+   *   hide: ['PartName'],
+   *   override: ['PartName'],
+   *   materials: {
+   *     'Boxcalf_Black': ['ToeCap'],
+   *   },
+   * },
+   */
+  getConfiguration() {
+    SHOP.customizer.getStepsData().forEach((step) => {
+      step.options.forEach((option) => {
+        let methodName = SHOP.customizer.getMethodName(option.type, 'getConf');
+        if (CUSTOMIZER_OPT_TYPES.includes(option.type) && typeof this[methodName] === 'function') {
+          this[methodName](step, option);
+        }
+      });
+    });
+  },
+
+  /**
+   * Set threedium configuration of TYPE_SIMPLE_MATERIAL type
+   * @param {object} step 
+   * @param {object} option 
+   */
+  getConfSimpleMaterial(step, option) {
+    // Fix. If the step hasn't change_part type
+    let optChangePart = SHOP.customizer.getStepOptionByType(step, TYPE_CHANGE_PART);
+    if (!optChangePart) {
+      this.addConfigMaterialPart(option.selectedValue, option.threediumGroupPart);
+      this.addConfigOverridePart(option.threediumGroupPart);
+    }
+  },
+
+  /**
+   * Set threedium configuration of TYPE_BURNISH type
+   * @param {object} step 
+   * @param {object} option 
+   */
+  getConfBurnish(step, option) {
+    if (SHOP.customizer.isBothValue(option.selectedValue)) {
+      this.addConfigOverridePart([BURNISH_TOECAP_PART, BURNISH_HEEL_PART]);
+    }
+    else if (!SHOP.customizer.isNoneValue(option.selectedValue)) {
+      this.addConfigOverridePart(option.selectedValue);
+    }
+  },
+
+  /**
+   * Set threedium configuration of TYPE_MEDALLION type
+   * @param {object} step 
+   * @param {object} option 
+   */
+  getConfMedallion(step, option) {
+    let optSimpleMaterial = SHOP.customizer.getStepOptionByType(step, TYPE_SIMPLE_MATERIAL);
+
+    if (!SHOP.customizer.isNoneValue(option.selectedValue)) {
+      this.addConfigOverridePart(option.selectedValue);
+
+      if (optSimpleMaterial) {
+        this.addConfigMaterialPart(optSimpleMaterial.selectedValue, option.selectedValue);
+      }
+    }
+  },
+
+  /**
+   * Set threedium configuration of TYPE_CHANGE_PART type
+   * @param {object} step 
+   * @param {object} option 
+   */
+  getConfChangePart(step, option) {
+    let optSimpleMaterial = SHOP.customizer.getStepOptionByType(step, TYPE_SIMPLE_MATERIAL);
+
+    if (!SHOP.customizer.isNoneValue(option.selectedValue)) {
+      this.addConfigOverridePart(option.selectedValue);
+
+      if (optSimpleMaterial) {
+        this.addConfigMaterialPart(optSimpleMaterial.selectedValue, option.selectedValue);
+      }
+    }
+  },
+
+  /**
+   * Set threedium configuration of TYPE_SOLE_TYPE type
+   * @param {object} step 
+   * @param {object} option 
+   */
+  getConfSoleType(step, option) {
+    // Per donar una part bona a la configuració s'agafen totes les parts i materials aplicats
+    // sobre Sole i Canto d'una tacada ja que son parts fraccionades entre varies opcions.
+    let self = SHOP.customizer,
+      stepCanto = self.getStepData(STEP_ID_CANTO),
+      optSoleColor = self.getStepOptionByType(step, TYPE_SOLE_COLOR),
+      optCantoColor = self.getStepOptionByType(stepCanto, TYPE_CANTO_COLOR),
+      soleCantoParts = self.getSoleAndCantoPartsFromSelectedOptions(step, option),
+      solePart = soleCantoParts.solePart,
+      cantoPart = soleCantoParts.cantoPart,
+      showParts = [solePart, cantoPart];
+
+    this.addConfigOverridePart(showParts);
+
+    if (optSoleColor) {
+      let material = optSoleColor ? optSoleColor.selectedValue : '';
+      this.addConfigMaterialPart(material, solePart);
+    }
+    if (optCantoColor) {
+      let material = optCantoColor ? optCantoColor.selectedValue : '';
+      this.addConfigMaterialPart(material, cantoPart);
+    }
+  },
+
+  /**
+   * Set threedium configuration of TYPE_VIRA_PICADO type
+   * @param {object} step 
+   * @param {object} option 
+   */
+  getConfViraPicado(step, option) {
+    let self = SHOP.customizer,
+      showParts = [],
+      optCantoColor = self.getStepOptionByType(step, TYPE_CANTO_COLOR),
+      viraPicadoMaterials = this.getViraPicadoMaterials(optCantoColor, option);
+
+    // Apply canto material to vira-picado & stormwelt
+    if (optCantoColor) {
+      if (viraPicadoMaterials.picado) {
+        this.addConfigMaterialPart(viraPicadoMaterials.picado, option.selectedValue);
+      }
+      if (viraPicadoMaterials.stormwelt) {
+        this.addConfigMaterialPart(viraPicadoMaterials.stormwelt, STORMWELT_PARAM);
+      }
+    }
+
+    // Add picado and stormwelt parts
+    if (!SHOP.customizer.isNoneValue(option.selectedValue)) {
+      showParts.push(option.selectedValue);
+    }
+    if (SHOP.customizer.existsOptionParam(option.params, STORMWELT_PARAM)) {
+      showParts.push(STORMWELT_PARAM);
+    }
+    this.addConfigOverridePart(showParts);
+  },
+
+  /**
+   * Set threedium configuration of TYPE_INSCRIPTION types
+   * @param {object} step
+   * @param {object} option
+   */
+  getConfInscription(step, option) {
+    let resetInscriptionOverlay = function () {
+      SHOP.customizer.threedium.updateOverlayText(option.threediumGroupPart, '');
+    };
+    this.onLoadCallbacks.push(resetInscriptionOverlay);
+  },
+
+  /**
+   * Set threedium configuration of TYPE_VAMP type
+   * @param {object} step
+   * @param {object} option
+   */
+  getConfVamp(step, option) {
+    // Change material part
+    this.addConfigMaterialPart(option.selectedValue, option.threediumGroupPart);
+    this.addConfigOverridePart(option.threediumGroupPart);
+
+    // Change culet material
+    let culetMaterial = SHOP.customizer.isNoneValue(option.params[2]) ? null : option.params[2];
+    if (culetMaterial) {
+      this.addConfigMaterialPart(culetMaterial, CULET_PART);
+    }
+
+    // Change culet overlay
+    let culetOverlay = SHOP.customizer.isNoneValue(option.params[3]) ? null : option.params[3];
+
+    if (culetOverlay) {
+      // No va el init :C
+      let updateCuletOverlay = function () {
+        SHOP.customizer.threedium.changeOverlay(CULET_PART, culetOverlay, '');
+      };
+      this.onLoadCallbacks.push(updateCuletOverlay);
+    }
+  },
+
+  /**
+   * Set threedium configuration of TYPE_INSCRIPTION_SOLE type
+   * @param {object} step
+   * @param {object} option
+   */
+  getConfInscriptionSole(step, option) {
+    let self = SHOP.customizer,
+      stepSole = step,
+      optSoleType = self.getStepOptionByType(stepSole, TYPE_SOLE_TYPE),
+      overlayName = self.isNoneValue(optSoleType.params[2]) ? null : optSoleType.params[2];
+
+    if (overlayName) {
+      let addSoleInscriptionOverlay = function () {
+        let stepSole = SHOP.customizer.getStepData(STEP_ID_SOLES),
+          soleCantoParts = SHOP.customizer.getSoleAndCantoPartsFromSelectedOptions(stepSole),
+          solePart = soleCantoParts.solePart;
+
+        SHOP.customizer.threedium.changeOverlay(solePart, overlayName);
+      };
+      this.onLoadCallbacks.push(addSoleInscriptionOverlay);
+    }
+  },
+
+  /**
+   * Add part into material key to materials threedium config object
+   * @param {string} material 
+   * @param {string|string[]} part 
+   */
+  addConfigMaterialPart(material, part) {
+    if (material.length) {
+      if (typeof this.configuration.parts.materials[material] == 'undefined') {
+        if (typeof part === 'string') {
+          this.configuration.parts.materials[material] = [part];
+        }
+        else {
+          this.configuration.parts.materials[material] = part;
+        }
+      }
+      else {
+        if (typeof part === 'string') {
+          this.configuration.parts.materials[material].push(part);
+        }
+        else {
+          this.configuration.parts.materials[material] = [...this.configuration.parts.materials[material], ...part]
+        }
+      }
+    }
+  },
+
+  /**
+   * Add part into overrite key to threedium config object
+   * @param {string[]|string} parts 
+   */
+  addConfigOverridePart(parts) {
+    if (Array.isArray(parts)) {
+      this.configuration.parts.override = [...this.configuration.parts.override, ...parts]
+    }
+    else if (typeof parts === 'string') {
+      this.configuration.parts.override.push(parts);
+    }
+  },
+};
+
+SHOP.customizer.threedium = { ...SHOP.customizer.threedium, ...module };
